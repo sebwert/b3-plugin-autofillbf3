f8db1237fedc959cd287543eff1cb84689fb45d0
Sebastian Ewert <me@sebastian-ewert.de>
2012-03-29 03:03:15 +0200

    added autostart/restart functionallity and map rotation change according to connected player

--- README.rst
+++ README.rst
@@ -10,13 +10,22 @@ connected player. The idea behind this is to get more quickmatch user.
 The initial forum thread can be found at:
 http://forum.bigbrotherbot.net/battle-field-3/script-for-server-filling/
 
+ - The available slots will always be 2 more than the connected player.
+ - If ``adjust_start_restart_payer_count`` is set to ``On`` in config it will
+   change roundStartPlayerCount/roundRestartPlayerCount to 8/4 after the
+   round started and set it to 1/0 when the round ends.
+ - If ``change_maps`` is set to ``On`` and you have two map files and 
+   write their correct absolute pathes to ``maps_many``, ``maps_few`` 
+   this plugin will change your map rotation when the number of connected user
+   falls under/growth over your ``maps_border``
 
-Installation
+
+ Installation
 ------------
 
 - copy extplugins stuff into b3/extplugins
 - add to the plugins section of your main b3 config file: 
-```<plugin name="autofillbf3" config="@b3/extplugins/conf/plugin_autofillbf3.xml" />```
+``<plugin name="autofillbf3" config="@b3/extplugins/conf/plugin_autofillbf3.xml" />``
 
 
 Config file
@@ -31,6 +40,49 @@ min_slots
 slots_when_empty
     if last user leaves your server the slots will be set to this value
 
+adjust_start_restart_payer_count
+    ''On/Off'' if enabled the roundStartPlayerCount/roundRestartPlayerCount
+    will be changed on rounf start and end
+
+change_maps
+    ''On/Off'' enables map according to the connected user
+
+maps_many
+    absolute path to your map file when many users connected
+
+maps_few
+    absolute path to your map file when few users connected
+
+maps_border
+    If less than this Number of user are connected maps_few is used
+
+Map Files:
+~~~~~~~~~~
+
+At this time it is important, that your map file does not contain any empty lines
+or any lines with another beginning as "mapname gamemode rounds". Only the 
+battlefield 3 server map names are allowed!
+
+MP_003 ConquestSmall0 1         #Theran Highway
+The line above is allowd cause the parser will stop after the number of rounds. 
+There you can write comments
+
+#Theran Highway  MP_003 ConquestSmall0 1
+The line above will lead to an error cause the parser is not able to recognise
+that the first element is not the map name
+
+You should have recieved a ``maps.dist`` file and a ``maps.all.txt`` file with 
+this plugin. The maps.dist file is an example of how an maps file could look like.
+No empty lines and the fomat described above. The maps.all.txt file is a file with
+all combinations of game modes and map names that are allowed in battlefield 3 at the
+time. please have a speacial look at the expansion set maps.
+
+Support
+-------
+
+Support will only be given at the bigbrother forum:
+http://forum.bigbrotherbot.net/releases/autofill-plugin-for-battlefield-3
+
 
 Changelog
 ---------
@@ -38,3 +90,8 @@ Changelog
 0.1
     basic functionality, listens for join, leave, round end, round
     start events and adjusts slots
+0.2
+    add mechanism to change roundStartPlayerCount/roundRestartPlayerCount
+0.3
+    add automatic map change according to connected player
+
--- extplugins/autofillbf3.py
+++ extplugins/autofillbf3.py
@@ -17,13 +17,15 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 #
-__version__ = '0.1'
+__version__ = '0.3'
 __author__ = 'Sebastian Ewert'
+
 
 import b3
 import b3.events
 from b3.plugin import Plugin
 from ConfigParser import NoOptionError
+from b3.parsers.frostbite2.protocol import CommandFailedError
 
 class Autofillbf3Plugin(Plugin):
     def __init__(self, console, config=None):
@@ -34,27 +36,39 @@ class Autofillbf3Plugin(Plugin):
         self._server_size = 16
         self._slots_when_empty = 16
         self._min_slots = 4
+        #change roundStart/RestartPlayerCount
+        self._do_rs = True
+        self._do_change_maps = False
+        self._maps = {'many' : [], 'few' : []}
+        self._maps_many = ''
+        self._maps_few = ''
+        self._maps_current_played = ''
+        self._maps_load_error = {'few' : True, 'many' : True}
+        self._maps_border = 10
+        self._maps_parsed = False
         Plugin.__init__(self, console, config)
 ################################################################################################################
 #
-# Plugin interface implementation
+# plugin interface implementation
 #
 ################################################################################################################
     def onLoadConfig(self):
-        """\
-This is called after loadConfig(). Any plugin private variables loaded
-from the config need to be reset here.
-"""
+        """
+        This is called after loadConfig(). Any plugin private variables loaded
+        from the config need to be reset here.
+        """
         self.verbose('Loading Autofill Config')
         self._loadServerSize()
         self._loadMinSlots()
         self._loadSlotsWhenEmpty()
+        self._loadDoRS()
+        self._loadMaps()
 
 
     def startup(self):
-        """\
-Initialize plugin settings
-"""
+        """
+        Initialize plugin settings
+        """
         # get the admin plugin so we can register commands
         self._adminPlugin = self.console.getPlugin('admin')
         if not self._adminPlugin:
@@ -66,74 +80,87 @@ Initialize plugin settings
         self.registerEvent(b3.events.EVT_GAME_ROUND_END)
         self.registerEvent(b3.events.EVT_CLIENT_DISCONNECT)
         self.registerEvent(b3.events.EVT_CLIENT_JOIN)
+
         
     def onEvent(self, event):
-        """\
-Handle intercepted events
-"""
+        """
+        Handle intercepted events
+        """
         if event.type == b3.events.EVT_GAME_ROUND_START:
-            self.debug('Gameround started set active to true and adjust slots')
-            self._active = True
-            self._adjustSlots()
+            self._doRoundStart()
         if event.type == b3.events.EVT_GAME_ROUND_END:
-            self.debug('Gameround end setting active false and maxPlayers to maximum size')
-            self._adjustSlots(True)
-            self._active = False
-        if (event.type == b3.events.EVT_CLIENT_DISCONNECT 
-                or event.type == b3.events.EVT_CLIENT_JOIN):
-            self._adjustSlots()
-        
+            self._doRoundEnd()
+        if event.type == b3.events.EVT_CLIENT_DISCONNECT:
+            self._doUserLeave()
+        if event.type == b3.events.EVT_CLIENT_JOIN:
+            self._doUserJoin()
 ################################################################################################################
 #
-# Privat Stuff
+# config loading
 #
 ################################################################################################################
+    def _doLoadVar(self, config_name, variable_name, func_suff = ""):
+        """
+        Load config name from config and save as self.variable_name
+        func_suff is for self.config.get + func_suff
+        """
+        try:
+            vars(self)[variable_name] = getattr(self.config, 'get' + func_suff)('preferences', config_name)
+        except NoOptionError:
+            self.info('No config option \"preferences\\%s\" found. Using default value : %s' % (config_name, getattr(self, variable_name)))
+        except ValueError, err:
+            self.debug(err)
+            self.warning('Could not read level value from config option \"preferences\\%s\". Using default value \"%s\" instead. (%s)' % (config_name, getattr(self, variable_name), err))
+        except Exception, err:
+            self.error(err)
+        self.info('%s is %s' % (config_name, getattr(self, variable_name)))
+    
+    
+    def _loadDoRS(self):
+        self._doLoadVar('adjust_start_restart_payer_count', '_do_rs', 'boolean')
     def _loadSlotsWhenEmpty(self):
-        """\
-Load slots to fall back when last user left from config field 
-"""
-        try:
-            self._slots_when_empty = int(self.config.get('preferences', 'slots_when_empty'))
-        except NoOptionError:
-            self.info('No config option \"preferences\\slots_when_empty\" found. Using default value : %s' % self._slots_when_empty)
-        except ValueError, err:
-            self.debug(err)
-            self.warning('Could not read level value from config option \"preferences\\slots_when_empty\". Using default value \"%s\" instead. (%s)' % (self._slots_when_empty, err))
-        except Exception, err:
-            self.error(err)
-        self.info('slots_when_empty is %s' % self._slots_when_empty)
+        self._doLoadVar('slots_when_empty', '_slots_when_empty', 'int')
     def _loadMinSlots(self):
-        """\
-Load minimum slots from config field 
-"""
-        try:
-            self._min_slots = int(self.config.get('preferences', 'min_slots'))
-        except NoOptionError:
-            self.info('No config option \"preferences\\min_slots\" found. Using default value : %s' % self._min_slots)
-        except ValueError, err:
-            self.debug(err)
-            self.warning('Could not read level value from config option \"preferences\\min_slots\". Using default value \"%s\" instead. (%s)' % (self._min_slots, err))
-        except Exception, err:
-            self.error(err)
-        self.info('min_slots is %s' % self._min_slots)
+        self._doLoadVar('min_slots', '_min_slots', 'int')
     def _loadServerSize(self):
-        """\
-Load server_size from config field 
-"""
-        try:
-            self._server_size = int(self.config.get('preferences', 'server_size'))
-        except NoOptionError:
-            self.info('No config option \"preferences\\server_size\" found. Using default value : %s' % self._server_size)
-        except ValueError, err:
-            self.debug(err)
-            self.warning('Could not read level value from config option \"preferences\\server_size\". Using default value \"%s\" instead. (%s)' % (self._server_size, err))
-        except Exception, err:
-            self.error(err)
-        self.info('server_size is %s' % self._server_size)
+        self._doLoadVar('server_size', '_server_size', 'int')
+    def _loadMaps(self):
+        self._doLoadVar('change_maps', '_do_change_maps', 'boolean')
+        self._doLoadVar('maps_many', '_maps_many')
+        self._doLoadVar('maps_few', '_maps_few')
+        self._doLoadVar('maps_border', '_maps_border', 'int')
+################################################################################################################
+#
+# event wrapper functions
+#
+################################################################################################################
+    def _doRoundStart(self):
+        self.debug('Gameround started set active to true and adjust slots')
+        self._active = True
+        self._adjustSlots()
+        if self._do_rs:
+            self._setRS(8, 4)
+    def _doRoundEnd(self):
+        self.debug('Gameround end setting active false and maxPlayers to maximum size')
+        self._adjustSlots(True)
+        self._active = False
+        if self._do_rs:
+            self._setRS(1, 0)
+    def _doUserJoin(self):
+        self._adjustSlots()
+        self._handleMaps()
+    def _doUserLeave(self):
+        self._adjustSlots()
+        self._handleMaps()
+################################################################################################################
+#
+# adjust server slots an start/restart stuff 
+#
+################################################################################################################
     def _adjustSlots(self, max_slots = False):
-        """\
-adjust slots according to rules and current logged in user
-"""
+        """
+        adjust slots according to rules and current logged in user
+        """
         if not self._active:
             self.debug("self.active is %s do not adjust player slots" % self._active)
             return True
@@ -149,9 +176,9 @@ adjust slots according to rules and current logged in user
             else:
                 self.debug("No slots changed, online/serversize  %s/%s" % (count_players, self._server_size))
     def _setSlots(self, count):
-        """\
-set server slots to count
-"""
+        """
+        set server slots to count
+        """
         now_slots = int(self.console.getCvar('maxPlayers').value)
         if(now_slots == count):
             self.debug("No slots changed, setting/wanted %s/%s" % (now_slots, count))
@@ -163,22 +190,116 @@ set server slots to count
                 self.error(err)
             else:
                 self.debug("maxPlayers set to %s" % count)
+    def _setRS(self, s, rs):
+        """
+        set s for vars.roundStartPlayerCount
+        and rs for vars.roundRestartPlayerCoun
+        """
+        try:
+            self.console.setCvar('roundStartPlayerCount', s)
+        except Exception, err:
+            self.error(err)
+        else:
+            self.debug("roundStartPlayerCount set to %s" % s)
+        try:
+            self.console.setCvar('roundRestartPlayerCount', rs)
+        except Exception, err:
+            self.error(err)
+        else:
+            self.debug("roundRestartPlayerCount set to %s" % rs)
+################################################################################################################
+#
+# handle maps stuff
+#
+################################################################################################################
+    def _handleMaps(self):
+        if not self._do_change_maps:
+            return True
+        #parse maps if not already parsed or forced to parse
+        if not self._maps_parsed:
+            self._parseMaps()
+        count_players = len(self.console.clients.getList())
+        if count_players > self._maps_border:
+            if self._maps_current_played == 'many':
+                self.debug('NO MAP LOADED: Loaded maps are %s, connected players %s, map border %s', (self._maps_current_played, count_players, self._maps_border))
+                return True
+            else:
+                self._setMaps('many')
+        else:
+            if self._maps_current_played == 'few':
+                self.debug('NO MAP LOADED: Loaded maps are %s, connected players %s, map border %s', (self._maps_current_played, count_players, self._maps_border))
+                return True
+            self._setMaps('few')
+    def _setMaps(self, map_type):
+        #exit if there was an error parsing this map type
+        if self._maps_load_error[map_type]:
+            self.debug('%s setMaps abortet because off previous map file error')
+            return True
+        try:
+            self.console.write(('mapList.clear',))
+        except CommandFailedError, e:
+            self.error('mapList.clear error, NO MAPS ADDED: %s' % e.message)
+        else:
+            self.debug('mapList cleared')
+            for map in self._maps[map_type]:
+                try:
+                    self.console.write(('mapList.add', map[0], map[1], map[2]))
+                except CommandFailedError, e:
+                    self.error('mapList.add: %s/%s/%s: %s' % (map[0], map[1], map[2], e.message))
+                else:
+                    self.debug('mapList.add: %s/%s/%s' % (map[0], map[1], map[2]))
+                    self._maps_current_played = map_type
+            try:
+                self.console.write(('mapList.setNextMapIndex', 0))
+            except CommandFailedError, e:
+                self.error('mapList.setNextMapIndex: %s' % e.message)
+    def _parseMaps(self):
+        try:
+            file_many = open(self._maps_many, "r").readlines()
+        except IOError:
+            self.error('can not open file "%s"' % self._maps_many)
+            self._maps_load_error['many'] = True
+        else:
+            for line in file_many:
+                self._maps['many'].append(line.split(' ', 3))
+            self._maps_load_error['many'] = False
+            self.debug('File %s succesfully opened' % file_many)
+        try:
+            file_few = open(self._maps_few, "r").readlines()
+        except IOError:
+            self.error('can not open file "%s"' % self._maps_few)
+            self._maps_load_error['few'] = True
+        else:
+            for line in file_few:
+                self._maps['few'].append(line.split(' ', 3))
+            self._maps_load_error['few'] = False
+            self.debug('File %s succesfully opened' % file_few)
+        if not self._maps_load_error['many'] and not self._maps_load_error['few']:
+            self._maps_parsed = True
+            self.info('There was at least one error opening your map files many/few %s/%s' % (self._maps_load_error['many'], self._maps_load_error['few']))
+################################################################################################################
+#
+# Debuging, Testing
+#
+################################################################################################################
+
+
 if __name__ == '__main__':
     from b3.config import XmlConfigParser
     from b3.fake import fakeConsole
     from b3.fake import joe, simon, moderator, superadmin, reg, admin
     import time
     config = XmlConfigParser() 
-    config.load('conf/plugin_autofillbf3.xml')
+    config.load('../conf/plugin_autofillbf3.xml')
     event_start = type('lamdbaobject', (object,), {'type' :  b3.events.EVT_GAME_ROUND_START})()
     event_end = type('lamdbaobject', (object,), {'type' :  b3.events.EVT_GAME_ROUND_END})()
     event_join = type('lamdbaobject', (object,), {'type' :  b3.events.EVT_CLIENT_JOIN})()
     event_leave = type('lamdbaobject', (object,), {'type' :  b3.events.EVT_CLIENT_DISCONNECT})()
     p = Autofillbf3Plugin(fakeConsole, config)
     p.console.setCvar('maxPlayers', 18)
-    
     p.onStartup()
     p.onLoadConfig()
+    """  Adjust slot size
     
     p.onEvent(event_start)
     joe.connects(cid = 1)
@@ -196,5 +317,22 @@ if __name__ == '__main__':
     joe.connects(cid = 1)
     p.onEvent(event_join)
     joe.disconnects()
+    p.onEvent(event_leave)
+    """
+    """ change map """
+    
+    p.onEvent(event_start)
+    joe.connects(cid = 1)
+    simon.connects(cid = 2)
+    moderator.connects(cid = 3)
+    p.onEvent(event_join)
+    superadmin.connects(cid=4)
+    p.onEvent(event_join)
+    reg.connects(cid=5)
+    p.onEvent(event_join)
+
+    joe.disconnects()
+    p.onEvent(event_leave)
+    admin.disconnects()
     p.onEvent(event_leave)
 
